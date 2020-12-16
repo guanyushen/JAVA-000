450fc99dd8a09ac4daa517d68c2aabb80246c3cc
guanys <1332112062@qq.com>
2020-12-16 17:23:27 +0800

    改造自定义RPC的程序，提交到github

---
+++ src/main/java/week09/rpc01/rpcfx-core/src/main/java/io/kimmking/rpcfx/api/RpcfxException.java
@@ -0,0 +1,34 @@
+package io.kimmking.rpcfx.api;
+
+import lombok.Data;
+
+/**
+ * @PROJECT_NAME: rpcfx
+ * @PACKAGE_NAME: io.kimmking.rpcfx.api
+ * @NAME: RpcfxException
+ * @Author: guanys
+ * @DATE: 12/16/20:2:54 PM
+ * @Description: TODO
+ * @Version: 1.0
+ **/
+@Data
+public class RpcfxException extends RuntimeException {
+
+    private String errCode;
+
+    private Object data;
+
+    public RpcfxException(){
+
+    }
+
+    public RpcfxException(String errMsg){
+        super(errMsg);
+    }
+
+    public RpcfxException(String errCode,String errMsg, Object data){
+        super(errMsg);
+        this.errCode = errCode;
+        this.data = data;
+    }
+}
